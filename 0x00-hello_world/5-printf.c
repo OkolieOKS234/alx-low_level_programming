fae4a3a1fa4964fdc6a3f5ab4c36f3ca151ce0f2
David Okolie <98519572+OkolieOKS234@users.noreply.github.com>
2022-09-09 22:09:55 +0100

    created 5-print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main entry point is main function as usual
+*
+* prints out a sentence and returns 0
+*/
+int main(void){
+printf("with proper grammar, but the outcome is a piece of art\n");
+  return(0);
+}
