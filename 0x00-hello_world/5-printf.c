63bfb64311564370878e65dab60d677762e8b851
David Okolie <98519572+OkolieOKS234@users.noreply.github.com>
2022-09-09 22:11:47 +0100

    created printf5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
+
 /**
-* main entry point is main function as usual
-*
-* prints out a sentence and returns 0
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
 */
-int main(void){
-printf("with proper grammar, but the outcome is a piece of art\n");
-  return(0);
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
