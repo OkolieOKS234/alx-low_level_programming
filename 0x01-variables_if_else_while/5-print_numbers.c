5309f15264a8f529fe8c7dda7aa022f2d8aada66
David Okolie <98519572+OkolieOKS234@users.noreply.github.com>
2022-09-11 16:18:12 +0100

    did 5-print_numbers.c task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+3 lines (19 sloc)  257 Bytes
+
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		++digit;
+	}
+	printf("\n");
+
+	return (0);
+}
